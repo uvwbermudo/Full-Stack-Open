46bb3b77379572005c2a7527480c7be7d7415f28
Bermudo, Urel <urelvan1@gmail.com>
2024-01-16 10:24:20 +0800

    Following Part 3B - Building Frontend and Deploying

--- follow-throughs/part3/backend/index.js
+++ follow-throughs/part3/backend/index.js
@@ -35,6 +35,7 @@ const unknownEndpoint = (request, response) => {
 app.use(cors())
 app.use(express.json())
 app.use(requestLogger)
+app.use(express.static('dist'))
 
 
 app.get('/', (request,response) => {
